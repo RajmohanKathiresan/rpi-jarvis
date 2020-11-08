287544a2ab887e66a523a4ec6c2fcd25c7adf477
Rajmohan Kathiresan <rajmohan.kathiresan@Impigertech.com>
2020-11-08 15:51:47 +0530

    Changed package for Temp senor to grove.seeed_dht

--- sense.py
+++ sense.py
@@ -1,7 +1,7 @@
 import time
 
 from grove.display.jhd1802 import JHD1802
-from grove.grove_temperature_humidity_sensor import DHT
+from grove.seeed_dht import DHT
 from grove.grove_light_sensor_v1_2 import GroveLightSensor
 from grove.grove_moisture_sensor import GroveMoistureSensor
 
