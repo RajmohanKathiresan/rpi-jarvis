f4a506d38d864375bfc4cd819b7b82e7afda0a77
Rajmohan Kathiresan <rajmohan.kathiresan@Impigertech.com>
2020-11-15 20:17:36 +0530

    Fixed logical issues

--- eye.py
+++ eye.py
@@ -6,8 +6,10 @@ from grove.grove_ryb_led_button import GroveLedButton
 
 
 def main():
+    print("Initializing")
+    now = int(time.time())
     # Grove - LED Button connected to port D5
-    button = GroveLedButton(5)
+    button = GroveLedButton(16)
 
     def on_event(index, event, tm):
         if event & Button.EV_SINGLE_CLICK:
@@ -16,14 +18,18 @@ def main():
             print(camera)
             camera.start_preview()
             time.sleep(2)
+            print(now)
             camera.capture('/tmp/picture1.jpg')
             camera.stop_preview()
     button.on_event = on_event
 
+    print("Registering event for button")
+
     while True:
+        now = int(time.time())
         time.sleep(1)
+        print(f"Time @  {now}")
+
 
 if __name__ == '__main__':
     main()
-
-
