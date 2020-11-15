2aa01276f743f5599288dd069ec55150774dd746
Rajmohan Kathiresan <rajmohan.kathiresan@Impigertech.com>
2020-11-15 20:11:00 +0530

    Capture picture on button click

--- eye.py
+++ eye.py
@@ -1,8 +1,27 @@
 from picamera import PiCamera
-from time import sleep
+from time
 
-camera = PiCamera()
-camera.start_preview()
-sleep(2)
-camera.capture('/tmp/picture1.jpg')
-camera.stop_preview()
+from grove.button import Button
+from grove.grove_ryb_led_button import GroveLedButton
+
+
+def main():
+    # Grove - LED Button connected to port D5
+    button = GroveLedButton(5)
+
+    def on_event(index, event, tm):
+        if event & Button.EV_SINGLE_CLICK:
+            camera = PiCamera()
+            camera.start_preview()
+            sleep(2)
+            camera.capture('/tmp/picture1.jpg')
+            camera.stop_preview()
+    button.on_event = on_event
+
+    while True:
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
+
+
